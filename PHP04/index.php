48aa909c72a1d6df732e414674b4b655ed8a03d8
JonasPrie <jonas.priedininkas@gmail.com>
2020-07-01 18:31:48 +0300

    PHP4 homework

---
+++ PHP04/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP4 homework</title>
+	</head>
+
+	<body>
+		<h1>PHP 04 homework</h1>
+		<?php
+			$cities = ['Berlynas', 'Roma', 'Londonas'];
+			$cities[] = 'Tokijas';
+				print_r($cities);
+		?>
+		<ul>
+			<li><?php echo $cities[1] ?></li>
+		</ul>
+		<?php
+			$cities2 = ['Tokijas' => '13.6', 'Vashingtonas' => '0.6', 'Maskva' => '11.5'];
+				print_r($cities2);
+			$cities2[] = ['Londonas' => '8.6'];
+		?>
+		<ul>
+
+			<?php
+				echo 'Gyventojų skaičius: ';
+				for ($i = 0; $i < 4; $i++) {
+					print_r($cities2['Tokijas'][$i]);
+				}
+				echo ' mln.';
+			?>
+		</ul>
+	</body>
+
+</html>
