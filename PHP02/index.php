a4106cb9f9ba95bc1c3ae68f5b5974aff11ab396
JonasPrie <jonas.priedininkas@gmail.com>
2020-07-01 18:29:57 +0300

    php2 homework

---
+++ PHP02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP2 homework</title>
+	</head>
+
+	<body>
+		<h1>PHP 02 homework</h1>
+		<?php
+			$country = 'Lietuva';
+			$city = 'Vilnius';
+			$population = '574221';
+			echo '<br>' . $city . ' - Lietuvos sostinė.';
+			echo '<br>' . "$country ir $city sparčiai auga.";
+		?>
+	</body>
+
+</html>
