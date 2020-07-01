b7d58138e74695d943e63f7427f3ac2ef8fe9892
JonasPrie <jonas.priedininkas@gmail.com>
2020-07-01 18:30:32 +0300

    php3 homework

---
+++ PHP03/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP3 homework</title>
+	</head>
+
+	<body>
+		<h1>PHP 03 homework</h1>
+		<?php
+			$x = 10;
+			$y = 7;
+			$z = $x + $y;
+			echo $x . ' + ' . $y . ' = ' . $z . '<br>';
+			$z = $x - $y;
+			echo $x . ' - ' . $y . ' = ' . $z . '<br>';
+			$z = $x * $y;
+			echo $x . ' * ' . $y . ' = ' . $z . '<br>';
+			$z = $x / $y;
+			echo $x . ' / ' . $y . ' = ' . $z . '<br>';
+			$z = $x % $y;
+			echo $x . ' % ' . $y . ' = ' . $z;
+		?>
+	</body>
+</html>
