5654e3b7f9e571cafcca9549608a83c27e9adc5b
JonasPrie <jonas.priedininkas@gmail.com>
2020-07-01 18:28:59 +0300

    PHP1 homework

---
+++ PHP01/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP1 homework</title>
+	</head>
+
+	<body>
+		<h1>PHP 01 homework</h1>
+		<?php
+			$country = 'Lithuania';
+			$city = 'Vilnius';
+			$population = '574221';
+		?>
+		<ul>
+			<li><?php echo $country; ?></li>
+			<li><?php echo $city; ?></li>
+			<li><?php echo $population; ?></li>
+		</ul>
+	</body>
+
+</html>
